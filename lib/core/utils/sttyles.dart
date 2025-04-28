8ff076e2696f6f94a3c19dc1e09eb1123b251e69
MohamedMadkour <madkourmohamed88@gmail.com>
2025-04-28 16:46:48 +0300

    add styles

--- lib/core/utils/sttyles.dart
+++ lib/core/utils/sttyles.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/widgets.dart';
 
 abstract class Styles {
-  static const TextStyle textStyle13 = TextStyle();
+  static const TextStyle textMedium13 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 13,
+    color: Color(0xffFFFFFF),
+  );
+  static const TextStyle textMedium10 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 10,
+    color: Color(0xffFFFFFF),
+  );
+  static const TextStyle textMedium9 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 9,
+    color: Color(0xffFFFFFF),
+  );
+  static const TextStyle textSemiBold21 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 21,
+    color: Color(0xffFFFFFF),
+  );
+  static const TextStyle textMedium14 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    color: Color(0xffFFFFFF),
+  );
 }
