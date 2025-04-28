b3166d1a552c6ed1c885e90f9e552a2a99f9e645
MohamedMadkour <madkourmohamed88@gmail.com>
2025-04-28 17:31:53 +0300

    add icons

--- lib/core/widgets/main/presentation/views/widget/custom_icon_navigation.dart
+++ lib/core/widgets/main/presentation/views/widget/custom_icon_navigation.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CustomIconNavigation extends StatelessWidget {
-  const CustomIconNavigation({super.key});
-
+  const CustomIconNavigation({super.key, required this.icon, this.onPressed});
+  final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {},
+      onPressed: onPressed,
       icon: Icon(Icons.home, size: 36, color: Color(0xff797979)),
     );
   }
