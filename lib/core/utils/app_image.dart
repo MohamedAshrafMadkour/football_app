59c420449156e46d4588297d44c81688b5eb22f7
MohamedMadkour <madkourmohamed88@gmail.com>
2025-04-28 15:42:19 +0300

    genrate images

---
+++ lib/core/utils/app_image.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBall
+  /// assets/images/ball.png
+  static const String imagesBall = "assets/images/ball.png";
+
+  /// Assets for imagesBundesliga
+  /// assets/images/bundesliga.png
+  static const String imagesBundesliga = "assets/images/bundesliga.png";
+
+  /// Assets for imagesLaliga
+  /// assets/images/laliga.png
+  static const String imagesLaliga = "assets/images/laliga.png";
+
+  /// Assets for imagesLigue1
+  /// assets/images/ligue1.png
+  static const String imagesLigue1 = "assets/images/ligue1.png";
+
+  /// Assets for imagesNos
+  /// assets/images/nos.png
+  static const String imagesNos = "assets/images/nos.png";
+
+  /// Assets for imagesPl
+  /// assets/images/pl.png
+  static const String imagesPl = "assets/images/pl.png";
+
+  /// Assets for imagesSeria
+  /// assets/images/seria.png
+  static const String imagesSeria = "assets/images/seria.png";
+}
+
