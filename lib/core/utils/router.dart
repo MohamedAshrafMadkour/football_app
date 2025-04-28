a9af224e04b5badf93b4bc87fb2b1a52c6f1e508
MohamedMadkour <madkourmohamed88@gmail.com>
2025-04-28 16:18:27 +0300

    add go_router package and create home view

---
+++ lib/core/utils/router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:football_scoore_app/feature/home/presentation/view/home_view.dart';
+import 'package:football_scoore_app/feature/splash/presentation/view/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class NavigationRouter {
+  static const kHomeView = '/homeView';
+  final GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const SplashView();
+        },
+      ),
+      GoRoute(
+        path: kHomeView,
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomeView();
+        },
+      ),
+    ],
+  );
+}
